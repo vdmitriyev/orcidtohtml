0b9fae02e4818d720afac443e64e36509f733d2f
Viktor Dmitriyev <dmitriyev.viktor@gmail.com>
2021-06-22 23:03:09 +0200

    FIX: UI for mobile devices + description

--- app/main/forms.py
+++ app/main/forms.py
@@ -6,7 +6,8 @@ from wtforms.validators import DataRequired, Length, Email, Regexp
 
 class OrcidForm(FlaskForm):
 
-    name    = StringField('Name',    validators=[Length(1, 50)])
-    orcidID = StringField('OrcidID', validators=[Regexp('^\d{4}-\d{4}-\d{4}-\d{4}', 
-                                                        message = Markup('<b class="text-danger">Wrong ORCID</b>'))])
+    name    = StringField('Name',               validators=[Length(2, 50,
+                                                            message = Markup('<b class="text-danger">Name field must be between %(min)d and %(max)d characters long</b>'))])
+    orcidID = StringField('ORCID (special ID)', validators=[Regexp('^\d{4}-\d{4}-\d{4}-\d{4}',
+                                                            message = Markup('<b class="text-danger">Wrong ORCID</b>'))])
     submit  = SubmitField('Generate')
